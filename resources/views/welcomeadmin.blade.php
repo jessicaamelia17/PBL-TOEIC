d3cce2a34218559bbf5aba657b297d2f515fa803
bagaskara04 <sagabdespair01@gmail.com>
2025-04-30 09:56:38 +0700

    push template admin

---
+++ resources/views/welcomeadmin.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts2.template')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Halo</h3>
+        <div class="card-tools"></div>
+    </div>
+    <div class="card-body">
+        Selamat datang semua
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LandingController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [LandingController::class, 'index']);
+Route::get('/admin', [WelcomeController::class, 'index'])->name('home');
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\RegistrasiController;
